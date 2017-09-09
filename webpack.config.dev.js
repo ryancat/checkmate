078365204dc691f9cc50db824fc9f7b24a775987
Ryan Chen <xin.chen@workday.com>
2017-09-08 17:47:31 -0700

    Setup environment

---
+++ webpack.config.dev.js
@@ -0,0 +1,28 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default {
+  devtool: 'inline-source-map',
+  entry: [
+    path.resolve(__dirname, 'src/main')
+  ],
+  target: 'web',
+  output: {
+    path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
+    publicPath: '/',
+    filename: 'main.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src')
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ],
+  module: {
+    loaders: [
+      {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader']},
+      {test: /(\.scss)$/, loaders: ['style-loader', 'css-loader', 'scss-loader']}
+    ]
+  }
+};
